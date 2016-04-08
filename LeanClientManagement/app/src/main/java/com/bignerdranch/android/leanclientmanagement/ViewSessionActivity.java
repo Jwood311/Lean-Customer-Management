632cb02ef39fc01f7fff876235ec16e92d9ca33e
Joshua Wood <jwood311@ymail.com>
2016-04-08 16:24:37 -0400

    centered some UI elemnts

---
+++ LeanClientManagement/app/src/main/java/com/bignerdranch/android/leanclientmanagement/ViewSessionActivity.java
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.leanclientmanagement;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Josh on 4/4/2016.
+ */
+public class ViewSessionActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session);
+
+    }
+}
