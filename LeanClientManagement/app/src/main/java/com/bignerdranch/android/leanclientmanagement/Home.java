8c3bb834be19a8b2d64a25127f35bc94ab51cb20
Joshua Wood <jwood311@ymail.com>
2016-04-08 13:18:39 -0400

    clean up

--- LeanClientManagement/app/src/main/java/com/bignerdranch/android/leanclientmanagement/Home.java
+++ LeanClientManagement/app/src/main/java/com/bignerdranch/android/leanclientmanagement/Home.java
@@ -1,13 +1,40 @@
 package com.bignerdranch.android.leanclientmanagement;
 
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class Home extends AppCompatActivity {
+public class Home extends FragmentActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            LoggedInFragment loggedFragment = new LoggedInFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            loggedFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, loggedFragment).commit();
+        }
+
+
     }
+
 }
---
+++ LeanClientManagement/app/src/main/java/com/bignerdranch/android/leanclientmanagement/LoggedInFragment.java
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.leanclientmanagement;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Josh on 4/4/2016.
+ */
+public class LoggedInFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_loggedin, container, false);
+    }
+}
