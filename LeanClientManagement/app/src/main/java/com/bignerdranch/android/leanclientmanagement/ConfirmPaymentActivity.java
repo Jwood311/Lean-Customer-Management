54c5d382f7beb7fb1dc91bc22269e397c2ec63e5
Joshua Wood <jwood311@ymail.com>
2016-04-09 14:46:55 -0400

    added buttons

---
+++ LeanClientManagement/app/src/main/java/com/bignerdranch/android/leanclientmanagement/ConfirmPaymentActivity.java
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.leanclientmanagement;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Josh on 4/7/2016.
+ */
+public class ConfirmPaymentActivity  extends FragmentActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_customer);
+
+    }
+}
