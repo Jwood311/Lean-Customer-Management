f3aadabfd4d4544b1a4857fb8b94112486002175
Joshua Wood <jwood311@ymail.com>
2016-04-09 10:56:40 -0400

    Additional layout

---
+++ LeanClientManagement/app/src/main/java/com/bignerdranch/android/leanclientmanagement/NewCustomerActivity.java
@@ -0,0 +1,18 @@
+package com.bignerdranch.android.leanclientmanagement;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Josh on 4/8/2016.
+ */
+public class NewCustomerActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_customer);
+
+    }
+
+}
+
