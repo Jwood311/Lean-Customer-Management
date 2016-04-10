8443580fb31553a8b72913e09d70a42946ee427b
Joshua Wood <jwood311@ymail.com>
2016-04-10 13:21:15 -0400

    fixed activities pointing to wrong layouts

--- LeanClientManagement/app/src/main/java/com/bignerdranch/android/leanclientmanagement/ReciptActivity.java
+++ LeanClientManagement/app/src/main/java/com/bignerdranch/android/leanclientmanagement/ReciptActivity.java
@@ -3,14 +3,13 @@ package com.bignerdranch.android.leanclientmanagement;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
-/**
- * Created by Josh on 4/7/2016.
- */
-public class ConfirmPaymentActivity  extends FragmentActivity{
+
+public class ReciptActivity extends FragmentActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm_payment);
+        setContentView(R.layout.activity_recipt);
 
     }
 }
